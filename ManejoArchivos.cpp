d2fb371588ae8b5a16eb8809a57e3bd70a646cda
Johan Sebastian Ussa Rubio <90299805+johanussa@users.noreply.github.com>
2022-10-12 17:27:48 -0500

    Mejora

--- ManejoArchivos.cpp
+++ ManejoArchivos.cpp
@@ -1,249 +1,267 @@
-#include <cstdlib>
 #include <iostream>
-#include <fstream>
-#include <string.h>
+#include <stdlib.h>	// Podremos salir del programa cuando ocurra un error 
+#include <fstream>	// Lectura y escritura de archivos
 #include <windows.h>
 
 using namespace std;
 
+int auxClave = 0;
+char mensaje[] = {'P','R','O','G','R','A','M','A','C','I','O','N', ' ','D','E',' ', 'S','O','F','T','W','A','R','E'};
+
+struct infoAp {
+	int clave;
+	int semestre;
+	int edad;
+	string nombre;
+	string grupo;	
+};
+struct infoAp aprendiz;
+
 void menu();
 void salir();
-void clear();
 void buscar();
 void guardar();
 void eliminar();
 void consultas();
-void modificar();
+//void modificar();
+void catchError();
 
-// Variables Globales
-
-bool encontrado=false;	// Saber cuando fue encontrado el registro
-int semestre = 0, edad = 0, clave = 0, auxClave = 0;
-char nombre[30], grupo[15], res[1], auxGrupo[10];
-char mensaje[]={'P','R','O','G','R','A','M','A','C','I','O','N', ' ','D','E',' ', 'S','O','F','T','W','A','R','E'};
-
-int main() {   
- 
-	menu(); 
-	system("PAUSE");
+int main() {
+	
+	menu();	
 	return 0;
 }
-void clear() {
-	
-	//system("CLS");
-	cout << "\n\n\n\t* Error, el Archivo No se Pudo Abrir o No ha sido Creado *\n\n\n\t"; 
-	//system("PAUSE");
-	//system("CLS");
-	
-	//return;
-}
-void menu() {   
-	system("CLS");
-	int opcion = 0;
-    do {    	
+void menu() {
+		
+	int opcion = 0;	
+    do {
+		system("CLS");    	
         cout << "\n\n\n\n\n \t\t\t Manejo de Archivos en C++\n\n";
-        cout << " \t\t\t\t 1. Guardar"<<endl;
-        cout << " \t\t\t\t 2. Eliminar"<<endl;
-        cout << " \t\t\t\t 3. Consultar Registros"<<endl;
-        cout << " \t\t\t\t 4. Buscar Registro"<<endl;
-        cout << " \t\t\t\t 5. Modificar"<<endl;
-        cout << " \t\t\t\t 6. Salir"<<endl<<endl;
-        cout << " \t\t\t Que deseas hacer.... ? ";
-        cin >> opcion;
-        system("CLS");
+        cout << " \t\t\t\t 1. Guardar" << endl;
+        cout << " \t\t\t\t 2. Eliminar" << endl;
+        cout << " \t\t\t\t 3. Consultar Registros" << endl;
+        cout << " \t\t\t\t 4. Buscar Registro" << endl;
+        cout << " \t\t\t\t 5. Modificar" << endl;
+        cout << " \t\t\t\t 6. Salir" << endl << endl;
+        cout << " \t\t\t Que deseas hacer.... ? "; cin >> opcion;
         
-        switch (opcion) {   
+        system("CLS");        
+        
+        switch (opcion) {           	
 			case 1: guardar(); break;
             case 2: eliminar(); break;
             case 3: consultas(); break;
             case 4: buscar(); break;
-            case 5: modificar(); break;  
+            case 5: /*modificar();*/ break;  
             case 6: salir(); break;
             default: 
 				cout << "\n\n\n\t\t\t* La Opcion ingresada es Incorrecta! *\n\n\t";
 				system("PAUSE");
 				system("CLS");
         }
+        system("PAUSE"); cout << "\n\n";
     } while (opcion != 6);
+    
+	return;
 }
-void guardar() {   
-
-	ofstream escritura;  ifstream consulta;   
-    escritura.open("aprendiz.txt", ios::out | ios::app); 
-	consulta.open("aprendiz.txt", ios::in);
-	bool repetido = false;
+void guardar() {
 	
-    if (escritura.is_open() && consulta.is_open()) {   
-    
-		cout << "\n\n\n\t> Ingresa la Clave del Aprendiz: ";
-        cin >> auxClave;
-        consulta >> clave;
-        
-        while (!consulta.eof()) {   
-			consulta >> nombre >> semestre >> grupo >> edad;
-            if(clave == auxClave) {   
-				cout << "Ya Existe un Registro con esta Clave" << endl;
-                repetido = true;
-                break;
-            }
-            consulta >> clave;
-        }
-        if (repetido == false) {  
-        
-			fflush(stdin); // cin.getline(Estu1.Nombre,50,'\n');
-			cout << "\t* Ingresa el nombre del Aprendiz: "; cin.getline(nombre, 30, '\n'); // gets(nombre);
-            cout << "\t* Ingresa el Semestre del Aprendiz: "; cin >> semestre; fflush(stdin);
-            cout << "\t* Ingresa el Grupo del Aprendiz: "; cin.getline(grupo, 15, '\n');
-            cout << "\t* Ingresa la Edad del Aprendiz: "; cin >> edad;
-            escritura << auxClave << " " << nombre << " " << semestre << " " << grupo << " \n\n"; // auxClave la tomo de la que solicite arriba
-            cout << "\n\t\t*** Registro Agregado Corectamente !!! ***\n\n\n\n\t";
-            system("PAUSE");
-        }		
-    } else { void clear(); }
-    escritura.close(); consulta.close();
+	int cont = 0;
+	string frase;
+	bool repeat = false;
+	
+	ofstream escribir;							// Crear tipo de dato ofstream de archivos - o = output
+	escribir.open("Aprendices.txt", ios::out | ios::app);	// Intentamos abrir el archivo - ios::out = abrir y reemplazar	
+		// Parametro 1: Nombre de archivo - Parametro 2: Modo de apertura - app = agregar datos
+		// Si el archivo No existe, lo va a crear
+	ifstream consultar;							// Crear tipo de dato ifstream de archivos - i = input
+	consultar.open("Aprendices.txt", ios::in);	// ios::in = lectura de datos
+	
+	if (escribir.is_open() && consultar.is_open()) {
+		
+		cout << "\n\n\t> Cuantos datos va a ingresar? "; cin >> cont; cout << endl;
+		system("CLS");
+		
+		for (int i = 0; i < cont; i++) {
+			
+			cout << "\n\t\t*** Datos aprendiz " << i + 1 << " ***\n\n";			
+			cout << "\t> Ingresa la Clave del Aprendiz: ";
+	        cin >> auxClave;
+	        consultar >> aprendiz.clave;
+	        
+	        while (!consultar.eof()) {   
+				consultar >> aprendiz.nombre >> aprendiz.semestre >> aprendiz.grupo >> aprendiz.edad;
+	            if(aprendiz.clave == auxClave) { 
+					system("CLS");  
+					cout << "\n\n\t\t* Ya Existe un Registro con esta Clave :( *" << endl;
+					cout << "\t\t\t* Vuelve a intentarlo *\n" << endl;
+	                repeat = true;
+	                break;
+	            }
+	            consultar >> aprendiz.clave;
+	        }
+			if (!repeat) {
+				
+				aprendiz.clave = auxClave;
+				fflush(stdin); 
+				cout << "\t* Ingresa el Nombre del Aprendiz: "; getline(cin, aprendiz.nombre); 
+				
+				for (int i = 0; i < aprendiz.nombre.length(); i++) {
+					if (aprendiz.nombre[i] == ' ') {
+						aprendiz.nombre = aprendiz.nombre.replace(aprendiz.nombre.find(" "), 1, "_");
+					}	
+				}				
+	            cout << "\t* Ingresa el Semestre del Aprendiz: "; cin >> aprendiz.semestre; fflush(stdin);
+	            cout << "\t* Ingresa el Grupo del Aprendiz: "; getline(cin, aprendiz.grupo);
+	            cout << "\t* Ingresa la Edad del Aprendiz: "; cin >> aprendiz.edad;
+	            escribir << aprendiz.clave << " " << aprendiz.nombre << " " << aprendiz.semestre
+					<< " " << aprendiz.grupo << " " << aprendiz.edad << "\n"; 
+			}			
+		}
+		cout << "\n\t\t*** Registro Agregado Corectamente !!! ***\n\n\n\n\t";
+		escribir.close(); consultar.close();
+				
+	} else { catchError(); }	
+	return;
 }
-void consultas() { 
-  
-    ifstream leer;
-	leer.open("aprendiz.txt", ios::out | ios::in);
-    
-	if (leer.is_open()) {   
-		cout << "\t\t\t\t\tRegistros del Archivo aprendiz.txt" << endl;
+void consultas() {
+	
+	ifstream read;
+	read.open("Aprendices.txt", ios::out | ios::in);
+	
+	if (read.is_open()) {
+		
+		cout << "\n\n\t\t\t\t    * Registros del archivo Aprendices.txt *" << endl;
         cout << "\t\t\t\t\t________________________________" << endl;
-        leer >> clave;
-        while (!leer.eof()) {   
-			leer >> nombre >> semestre >> grupo >> edad;
-            cout << "\t\t\t\t\t* Clave: " << clave << endl;
-            cout << "\t\t\t\t\t* Nombre: " << nombre << endl;
-            cout << "\t\t\t\t\t* Semestre: " << semestre << endl;
-            cout << "\t\t\t\t\t* Grupo: " << grupo << endl;
-            cout << "\t\t\t\t\t* Edad: " << edad << endl;
-            leer >> clave;
+        read >> aprendiz.clave;
+        
+        while (!read.eof()) {   
+			read >> aprendiz.nombre >> aprendiz.semestre >> aprendiz.grupo >> aprendiz.edad;
+            cout << "\n\t\t\t\t\t * Clave: " << aprendiz.clave << endl;
+            cout << "\t\t\t\t\t * Nombre: " << aprendiz.nombre << endl;
+            cout << "\t\t\t\t\t * Semestre: " << aprendiz.semestre << endl;
+            cout << "\t\t\t\t\t * Grupo: " << aprendiz.grupo << endl;
+            cout << "\t\t\t\t\t * Edad: " << aprendiz.edad << endl;
+            read >> aprendiz.clave;
             cout << "\t\t\t\t\t________________________________" << endl;
         }
-    } else { cout << "Entra"; system("pause"); void clear(); }
-    
-    leer.close();
+        cout << "\n\n\n\t";
+	} else { catchError(); }
+	
+	read.close();
+	return;
 }
-void eliminar() {   
-
-	ofstream aux; 	// para escribir en un archivo auxiliar que nos va a servir para ir vaciando los registros que tiene el original
-    ifstream leer;	// para solo leer, al final aux  va a tener todo lo de el original mas la modificacion,
-					// para finalmente eliminar el original y luego renombrar el aux con el nombre del originaal
-    encontrado = false;
-    int auxClave = 0;
-    
-    aux.open("auxiliar.txt", ios::out); 
-	leer.open("aprendiz.txt", ios::in);
-    
-    if (aux.is_open() && leer.is_open()) {   
-    
-		cout << "Ingresa la clave del Aprendiz que desea Eliminar" << endl;
-        cin >> auxClave;
-        leer >> clave;
-        
-        while (!leer.eof()) {   
-			leer >> nombre >> semestre >> grupo >> edad;
-            if (auxClave == clave)	{	// Comparar cada registro para ver si es encontrado
-				encontrado = true;      
-                cout << "Registro Eliminado " << endl;
-            } else {   
-				aux << clave << " " << nombre << " " << semestre << " " << grupo << " \n\n";
-            }
-            leer >> clave;//leer adelantada
-        }
-    } else { void clear(); }
-    
-    if (encontrado == false) {
-        cout << "No se encontro ningun registro con clave : " << auxClave << endl;	
-    }
-	aux.close(); 
-    leer.close(); 
-    remove("aprendiz.txt");
-    rename("auxiliar.txt","aprendiz.txt");
+void buscar() {
+	
+	bool find = false;
+	
+	ifstream read;
+	read.open("Aprendices.txt", ios::out | ios::in);	
+	
+	if (read.is_open()) {
+		
+		cout << "\n\n\t\t* Ingrese la Clave del aprendiz a buscar: "; cin >> auxClave;
+		read >> aprendiz.clave;
+		
+		while (!read.eof()) {
+			read >> aprendiz.nombre >> aprendiz.semestre >> aprendiz.grupo >> aprendiz.edad;
+			if (aprendiz.clave == auxClave) {
+				cout << "\n\t\t\t______________________________" << endl;
+                cout << "\n\t\t\t * Clave: " << aprendiz.clave << endl;
+                cout << "\t\t\t * Nombre: " << aprendiz.nombre << endl;
+                cout << "\t\t\t * Semestre: " << aprendiz.semestre << endl;
+                cout << "\t\t\t * Grupo: " << aprendiz.grupo << endl;
+                cout << "\t\t\t * Edad: " << aprendiz.edad << endl;
+                cout << "\t\t\t______________________________\n\n\n\t";
+                find = true;
+			}
+			read >> aprendiz.clave;
+		}
+		if (!find) { 
+			system("CLS");
+			cout << "\n\n\n\t*** No existe el registro para la clave " << auxClave << " ***\n\n\t"; 
+		}
+	} else { catchError(); }
+	
+	read.close();	
+	return;
 }
-void buscar() {    
-
-    ifstream leer;	// Creamos la variable de tipo leer
-    leer.open("aprendiz.txt", ios::out | ios::in);	// Abrimos el archivo
-    encontrado = false;	// validando la apertura del archivo
-    
-    if (leer.is_open()) { 
-	  
-		cout << "Ingresa la Clave del Aprendiz que deseas Buscar: ";
-        cin >> auxClave;
-        leer >> clave;	// leer adelantada
+void eliminar() {
+	
+	int opc = 0;
+	bool find = false;
+	
+	ofstream archAux;
+	ofstream read;
+	
+	archAux.open("Auxiliar.txt", ios::out);
+	read.open("Aprendices.txt", ios::out | ios::in);
+	
+	if (archAux.is_open() && read.is_open()) {
+		
+		cout << "\n\n\t\t* Ingrese la Clave del aprendiz a buscar: "; cin >> auxClave;
+		read >> aprendiz.clave;
+		
+		while (!read.eof()) {
 			
-        while(!leer.eof()) {   
-			leer >> nombre >> semestre >> grupo >> edad;//leyendo los campos del registro
-            if (auxClave == clave)	{   // Comparar cada registro para ver si es encontrado
-				cout << "\t\t\t______________________________" << endl;
-                cout << "\t\t\t\t* Clave: " << clave << endl;
-                cout << "\t\t\t\t* Nombre: " << nombre << endl;
-                cout << "\t\t\t\t* Semestre: " << semestre << endl;
-                cout << "\t\t\t\t* Grupo: " << grupo << endl;
-                cout << "\t\t\t\t* Edad: " << edad << endl;
-                cout << "\t\t\t______________________________" << endl;
-                encontrado = true;
-            }
-            leer >> clave; // leer adelantada
-        }
-        if (!encontrado) {   cout << "No hay registros con la Clave " << auxClave << endl; }
-    } else { void clear(); }
-    
-    leer.close();	// Cerrando el archivo	    
-}
-void modificar() {   
-
-	ofstream aux;	// para escribir en un archivo auxiliar que nos va a servir para ir vaciando los registros que tiene el original
-    ifstream leer;	// para solo leer, al final aux  va a tener todo lo de el original mas la modificacion,
-	encontrado = false;	// para finalmente eliminar el original y luego renombrar el aux con el nombre del original  
-	  
-    int auxClave = 0;
-    char auxNombre[20];
-    aux.open("auxiliar.txt",ios::out);	leer.open("aprendiz.txt",ios::in);
-    
-    if(aux.is_open() && leer.is_open()) {   
-    
-		cout << "Ingresa la clave del Aprendiz que desea modificar" << endl;
-        cin >> auxClave;
-        leer >> clave;
-        
-        while(!leer.eof()) {	// Mientras no llegue al final del archivo  
-		 
-			leer >> nombre >> semestre >> grupo >> edad;
-            if (auxClave == clave) {	// Comparar cada registro para ver si es encontrado   
-				encontrado = true;
-				cout << "\t\t\t______________________________" << endl;
-                cout << "\t\t\t\t* Clave: " << clave << endl;
-                cout << "\t\t\t\t* Nombre: " << nombre << endl;
-                cout << "\t\t\t\t* Semestre: " << semestre << endl;
-                cout << "\t\t\t\t* Grupo: " << grupo << endl;
-                cout << "\t\t\t\t* Edad: " << edad << endl;
-                cout << "\t\t\t______________________________" << endl;
-                cout << "Ingrese el nuevo nombre del aprendiz con clave : " << clave << endl;
-                cin >> auxNombre;
-                aux << clave << " " << auxNombre << " " << semestre << " " << grupo << " " << edad << endl;
-                cout << "Registro Modificado " << endl;
-            } else {   
-				aux << clave << " " << nombre << " " << semestre << " " << grupo << " \n\n";
-            }
-            leer >> clave;	// leer adelantada
-        }
-    } else { void clear(); }
-    
-    if(encontrado == false) { cout << "No se encontro ningun registro con clave : " << auxClave << endl; }
-    
-    leer.close();
-    aux.close();
-    remove("aprendiz.txt");
-    rename("auxiliar.txt","aprendiz.txt");	
+			read >> aprendiz.nombre >> aprendiz.semestre >> aprendiz.grupo >> aprendiz.edad;
+			
+			if (aprendiz.clave == auxClave) {
+				
+				do {
+					system("CLS");
+					cout << "\n\n\t\t*** Seguro desea eliminar el registro " << auxClave << " ***\n";
+					cout << "\n\t\t * Si, Eliminar registro ....... 1";
+					cout << "\n\t\t * No, No eliminarlo ........... 2";
+					cout << "\n\t\t * Cancelar Operacion .......... 3";
+					cout << "\n\t\t\t>>> Opcion: "; cin >> opc;
+					
+					switch (opc) {
+						system("CLS");
+						case 1: 
+							find = true;
+							cout << "\n\n\t\t*** El registro de " << aprendiz.nombre << " ha sido Eliminado ***\n\n";
+							break;
+						case 2: return; break;
+						case 3: return; break;
+						default: 							
+							cout << "\n\n\t\t*** !! La Opcion ingresada es incorrecta ¡¡\n";
+							cout << "\t\t*** Vuelva a intentarlo :) ***\n\n\n";
+					}
+				} whiile (opc < 1 || opc > 3); 
+			} else {
+				archAux << aprendiz.clave << " " << aprendiz.nombre << " " << aprendiz.semestre << " "
+					<< aprendiz.grupo << " " << aprendiz.edad << endl;
+			}
+			read >> aprendiz.clave;
+		}
+	} else { catchError(); }
+	
+	if (!find) {
+		system("CLS");
+		cout << "\n\n\n\t*** No existe el registro para la clave " << auxClave << " ***\n\n\t"; 
+	}
+	read.close();
+	archAux.close();
+	remove("Aprendices.txt");
+	rename("Auxiliar.txt", "Aprendices.txt");
+	
+	return;
 }
 void salir() {   
-    cout << "\n\n\t\t\t   ";
-    for(int i = 0; i < 29; i++) {   
-		cout<<mensaje[i];
+
+    cout << "\n\n\n\t\t\t *** ";
+    for(int i = 0; i < strlen(mensaje); i++) {   
+		cout << mensaje[i];
         Sleep(80);
     }
-    cout << "\n\n\t\t\t      Programa Finalizado\n\n\n\n\n\n\n\n\n\n";
+    cout << " ***\n\n\t\t\t   *** Programa Finalizado ***\n\n\n\n\n\n\n\t\t";
+    return;
+}
+void catchError() {
+	
+	cout << "\n\n\t\t** El archivo NO pudo ser creado o abierto **\n\n";
+	cout << "\t\t\t*** Vuelve a intentarlo :( ***\n\n";
+	exit(1);	// Salimos del programa ya que no se pudo crear el archivo	
+	return;
 }
